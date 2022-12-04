05035c485fdbcc6d2efe5a53f270cea6878636c6
DenseLance <denselance@gmail.com>
2022-12-04 18:31:53 +0800

    Updated CBC mode for HIGHT
    Variable name corrected. Added CBC mode.

--- hight.py
+++ hight.py
@@ -1,16 +1,3 @@
-# TEST CASE
-MK = [0x88, 0xE3, 0x4F, 0x8F, 0x08, 0x17, 0x79, 0xF1, 0xE9, 0xF3, 0x94, 0x37, 0x0A, 0xD4, 0x05, 0x89]
-P = [0xD7, 0x6D, 0x0D, 0x18, 0x32, 0x7E, 0xC5, 0x62]
-expected_C = [0xE4, 0xBC, 0x2E, 0x31, 0x22, 0x77, 0xE4, 0xDD]
-
-# MAIN CODE
-print("Plaintext:", [hex(byte)[2:].upper() for byte in P])
-
-assert len(P) == 8
-assert all(0 <= byte <= 0xFF for byte in P)
-assert len(MK) == 16
-assert all(0 <= byte <= 0xFF for byte in MK)
-
 def list_to_byte(lst):
     byte = 0
     for bit in lst:
@@ -140,7 +127,7 @@ def decryption_final_transformation(X_32, WK):
     ]
     return D
 
-def encryption_tranformation(P, WK, SK):
+def encryption_transformation(P, WK, SK):
     X_i = encryption_initial_transformation(P, WK)
     for i in range(32):
         X_i = encryption_round_function(i, X_i, SK)
@@ -156,22 +143,10 @@ def decryption_transformation(C, WK, SK):
 
 def hight_encryption(P, MK):
     WK, SK = encryption_key_schedule(MK)
-    C = encryption_tranformation(P, WK, SK)
+    C = encryption_transformation(P, WK, SK)
     return C
 
 def hight_decryption(C, MK):
     WK, SK = decryption_key_schedule(MK)
     D = decryption_transformation(C, WK, SK)
     return D
-
-C = hight_encryption(P, MK)
-
-print("Encrypted bytes:", [hex(byte)[2:].upper() for byte in C])
-
-assert C == expected_C
-
-D = hight_decryption(C, MK)
-
-print("Decrypted bytes:", [hex(byte)[2:].upper() for byte in D])
-
-assert D == P
